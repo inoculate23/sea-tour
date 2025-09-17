54ebd730ceaa0fac8b64a8a6f8cefe6dcee9e56f
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2025-09-17 10:31:15 -0700

    Fix className attribute in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,11 +8,11 @@ const HeroSection: React.FC = () => {
   return (
     
     
-    <section classNam="relative min-h-screen flex items-center justify-center overflow-hidden">
+    <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
       {/* Background Image with Overlay */}
 
-     <div class="sketchfab-embed-wrapper"> 
-   <iframe title="Thai Monkey Mask" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/cbdc5fe616f946e0950ad6f01cba5e53/embed?autostart=1&transparent=1&ui_theme=dark"> 
+     <div className="sketchfab-embed-wrapper"> 
+   <iframe title="Thai Monkey Mask" frameBorder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/cbdc5fe616f946e0950ad6f01cba5e53/embed?autostart=1&transparent=1&ui_theme=dark"> 
  </iframe> 
     </div>
       
