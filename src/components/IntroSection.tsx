b8838b77c139da82423841851fb30650b788499b
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2025-09-18 05:38:54 -0700

    Refactor IntroSection layout by removing scroll indicator
    Removed scroll indicator and adjusted main content div.

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -8,16 +8,8 @@ const IntroSection: React.FC = () => {
   return (
  <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-dark-bg">
 
-
-  {/* Scroll Indicator */}
-  <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-20 animate-bounce">
-    <ArrowDown className="w-8 h-8 text-haawke-gold" />
-  </div>
-
-
-
       {/* Main Content */}
-        <div className="mb-12 h-[100vh] w-full">
+        <div className="h-[100vh] w-full">
     <iframe src="https://cheery-starship-67c0f5.netlify.app" className="mb-12 h-[100vh] w-full"></iframe>
 
      
