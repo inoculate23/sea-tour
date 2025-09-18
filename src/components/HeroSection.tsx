a14d2f67c80e92c4b70d5145954be397f9642114
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2025-09-18 04:15:50 -0700

    Add iframe and scroll indicator to HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,6 +15,14 @@ const HeroSection: React.FC = () => {
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
+
+        <iframe src="https://cheery-starship-67c0f5.netlify.app" width="100%" height="100vh"></iframe>
+      
+            {/* Scroll Indicator */}
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-20 animate-bounce">
+        <ArrowDown className="w-8 h-8 text-haawke-gold" />
+      </div>
+      </div>
       />
       
       {/* Animated Background Particles */}
